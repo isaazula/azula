d014bd6981de0fa85eb7cfc76c0fda19ebf7f08b
Carlos <casalgado86@gmail.com>
2024-02-08 17:14:27 -0500

    fix category bug

--- src/components/sections/TheCategories.vue
+++ src/components/sections/TheCategories.vue
@@ -6,68 +6,77 @@ const router = useRouter();
 
 <template>
   <section class="categories">
-    <video
-      class="video-categories"
-      id="v2"
-      key="v2"
-      muted
-      autoplay
-      loop
-      @click="
-        () => {
-          router.push({ name: 'gallery', params: { category: 'photography' } });
-        }
-      "
-    >
-      <source
-        src="https://firebasestorage.googleapis.com/v0/b/koftafusion.appspot.com/o/videos_isa%2Fsecond.mp4?alt=media&token=afb82339-b035-4c96-9bb1-9b7bf70aea02"
-        type="video/mp4"
-      />
-    </video>
+    <div>
+      <video
+        class="video-categories"
+        id="v2"
+        key="v2"
+        muted
+        autoplay
+        loop
+        @click="
+          () => {
+            router.push({
+              name: 'gallery',
+              params: { category: 'photography' },
+            });
+          }
+        "
+      >
+        <source
+          src="https://firebasestorage.googleapis.com/v0/b/koftafusion.appspot.com/o/videos_isa%2Fsecond.mp4?alt=media&token=afb82339-b035-4c96-9bb1-9b7bf70aea02"
+          type="video/mp4"
+        />
+      </video>
+    </div>
 
-    <video
-      class="video-categories"
-      id="v3"
-      key="v3"
-      muted
-      autoplay
-      loop
-      @click="
-        () => {
-          router.push({
-            name: 'gallery',
-            params: { category: 'illustration' },
-          });
-        }
-      "
-    >
-      <source
-        src="https://firebasestorage.googleapis.com/v0/b/koftafusion.appspot.com/o/videos_isa%2Fthird.mp4?alt=media&token=81782524-7335-4216-a0f0-0c3f4abb7e37"
-        type="video/mp4"
-      />
-    </video>
+    <div>
+      <video
+        class="video-categories"
+        id="v3"
+        key="v3"
+        muted
+        autoplay
+        loop
+        @click="
+          () => {
+            router.push({
+              name: 'gallery',
+              params: { category: 'illustration' },
+            });
+          }
+        "
+      >
+        <source
+          src="https://firebasestorage.googleapis.com/v0/b/koftafusion.appspot.com/o/videos_isa%2Fthird.mp4?alt=media&token=81782524-7335-4216-a0f0-0c3f4abb7e37"
+          type="video/mp4"
+        />
+      </video>
+    </div>
 
-    <video
-      class="video-categories"
-      id="v1"
-      key="v1"
-      muted
-      autoplay
-      loop
-      @click="
-        () => {
-          router.push({
-            name: 'gallery',
-            params: { category: 'animation' },
-          });
-        }
-      "
-    >
-      <source
-        src="https://firebasestorage.googleapis.com/v0/b/koftafusion.appspot.com/o/videos_isa%2Ffirst.mp4?alt=media&token=8959d4ef-23ff-481e-88ad-4843105e0691"
-        type="video/mp4"
-      />
-    </video>
+    <div>
+      <video
+        class="video-categories"
+        id="v1"
+        key="v1"
+        muted
+        autoplay
+        loop
+        @click="
+          () => {
+            router.push({
+              name: 'gallery',
+              params: { category: 'animation' },
+            });
+          }
+        "
+      >
+        <source
+          src="https://firebasestorage.googleapis.com/v0/b/koftafusion.appspot.com/o/videos_isa%2Ffirst.mp4?alt=media&token=8959d4ef-23ff-481e-88ad-4843105e0691"
+          type="video/mp4"
+        />
+      </video>
+    </div>
   </section>
 </template>
 
--- src/components/sections/TheVideo.vue
+++ src/components/sections/TheVideo.vue
@@ -3,7 +3,10 @@
 <template>
   <section>
     <video class="video" muted autoplay loop>
-      <source src="" type="video/mp4" />
+      <source
+        src="https://firebasestorage.googleapis.com/v0/b/koftafusion.appspot.com/o/videos_isa%2FVideo%20Final%20%C2%B7%20Isabella%20Azula.mp4?alt=media&token=9f715b1e-b0c3-4d69-ad5d-2a0c61869ecf"
+        type="video/mp4"
+      />
     </video>
   </section>
 </template>
